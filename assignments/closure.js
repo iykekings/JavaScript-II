8c5c4aeace32b5a45ecc868c8cfe3d1045b2e479
Ikechukwu Eze <iykekings36@gmail.com>
2019-05-07 13:58:30 +0100

    mvp complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,13 +1,21 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function add(x) {
+  return function(y) {
+    return x + y;
+  };
+}
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
-
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function() {
+    count++;
+    return count;
+  };
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
@@ -18,4 +26,22 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  return {
+    increment: function() {
+      count++;
+      return count;
+    },
+    decrement: function() {
+      count--;
+      return count;
+    }
+  };
 };
+
+// Example usage: const factory = counterFactory();
+// factory.increment(); // 1
+// factory.increment(); // 2
+// factory.decrement(); // 1
+// factory.decrement(); // 0
+// factory.decrement(); // -1
